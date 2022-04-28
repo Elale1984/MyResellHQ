8089a948115cfacdbb8eff422afdcbde665894ed
elale1984 <elale@mygcu.edu>
2022-04-28 02:36:10 -0500

    Initial commit-created drawer. created welcome page

---
+++ app/src/main/java/com/heady/myresellhq/ui/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.heady.myresellhq.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heady.myresellhq.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
