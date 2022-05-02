4462816087b43be373d25e49fd90a1bebc0b4cb9
elale1984 <elale@mygcu.edu>
2022-05-01 22:49:27 -0500

    Basics for on click handling for LogIn.kt and Register.kt finished without error handling. Navigation between activities handled

--- app/src/main/java/com/heady/myresellhq/ui/LogIn.kt
+++ app/src/main/java/com/heady/myresellhq/ui/LogIn.kt
@@ -1,12 +1,36 @@
 package com.heady.myresellhq.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.heady.myresellhq.MainActivity
 import com.heady.myresellhq.R
+import com.heady.myresellhq.databinding.ActivityLogInBinding
+import com.heady.myresellhq.databinding.ActivityWelcomeScreenBinding
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnSignIn.setOnClickListener {
+            Toast.makeText(this, "Sign In Clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tbtnSignUp.setOnClickListener {
+            Toast.makeText(this, "Sign Un Clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
